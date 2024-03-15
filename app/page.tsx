d0d8a0371cd81caebfc3f770f8b00d4159dd6962
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-03-15 15:19:06 -0500

    feat 🎉 develop dashboard functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,44 @@
-import Image from "next/image";
-import { ThemeToggle } from "./components/Themetoggle";
 import { Button } from "@/components/ui/button";
 import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <section className="flex items-center justify-center bg-background h-[90vh]">
-      <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
-        <div className="max-w-3xl mx-auto text-center">
-          <div>
-            <span className="w-auto px-6 py-3 rounded-full bg-secondary">
-              <span className="text-sm font-medium text-primary">
-                Sort your notes easily
+export default async function Home() {
+  const { isAuthenticated } = getKindeServerSession();
+
+  if (await isAuthenticated()) {
+    return redirect("/dashboard")
+  }
+    return (
+      <section className="flex items-center justify-center bg-background h-[90vh]">
+        <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+          <div className="max-w-3xl mx-auto text-center">
+            <div>
+              <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+                <span className="text-sm font-medium text-primary">
+                  Sort your notes easily
+                </span>
               </span>
-            </span>
 
-            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
-              Create Note with ease
-            </h1>
+              <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
+                Create Note with ease
+              </h1>
 
-            <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              similique repudiandae consectetur libero corporis?
-            </p>
-          </div>
+              <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                similique repudiandae consectetur libero corporis?
+              </p>
+            </div>
 
-          <div className="flex justify-center max-w-sm mx-auto mt-10">
-            <RegisterLink>
-              <Button size="lg" className="w-full">
-                Sign Up for free
-              </Button>
-            </RegisterLink>
+            <div className="flex justify-center max-w-sm mx-auto mt-10">
+              <RegisterLink>
+                <Button size="lg" className="w-full">
+                  Sign Up for free
+                </Button>
+              </RegisterLink>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
 }
