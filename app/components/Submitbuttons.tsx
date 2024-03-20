d21cd5390605ecff91473ea2f69532d6526522b3
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-03-20 07:24:25 -0500

    feat 🎉 implement note creation and build dashboard homepage

--- app/components/Submitbuttons.tsx
+++ app/components/Submitbuttons.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
+import { Loader2, Trash } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 export default function SubmitButton() {
@@ -33,6 +33,42 @@ export function StripeSubscriptionCreationButton() {
       ) : (
         <Button type="submit" className="w-full">
           Create Subscription
+        </Button>
+      )}
+    </>
+  );
+}
+
+export function StripeSubscriptionUpdateButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full">
+          View payment details
+        </Button>
+      )}
+    </>
+  );
+}
+
+export function TrashDelete() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled variant={"destructive"} size="icon">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+        </Button>
+      ) : (
+        <Button type="submit" variant={"destructive"} size="icon">
+          <Trash className="h-4 w-4" />
         </Button>
       )}
     </>
--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -20,9 +20,10 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, unstable_noStore as noStore } from "next/cache";
 
 async function getData(userId: string) {
+  noStore()
   const data = await prisma.user.findUnique({
     where: {
       id: userId,
